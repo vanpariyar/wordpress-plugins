00faaeb2d771e6a741fb6274f892a351579a6ef2
Ronak Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2026-07-13 15:27:56 +0530

    Update

--- plugins/sahajanand-post-to-speech/build/index.asset.php
+++ plugins/sahajanand-post-to-speech/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5e8f91820135f0d56bc8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2495d28ebcd05ea37265');
